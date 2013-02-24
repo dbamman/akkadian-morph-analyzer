4b440f18dcc8ebb268507631b13c758b75a44a1f
David Bamman <dbamman@cs.cmu.edu>
2013-02-24 14:53:24 -0500

    init

---
+++ scripts/parse.py
@@ -0,0 +1,58 @@
+import sys,re,math,operator,json
+import numpy as np
+from random import choice
+
+targets=[]
+name=""
+
+class bcolors:
+
+    REG = '\033[93m'
+    PARSE = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.REG = ''
+        self.PARSE = ''
+        self.ENDC = ''
+
+def main(parseFile, filename):
+
+    # read morphological analyses
+    parses={}
+    file=open(parseFile)
+    for line in file:        
+        cols=line.rstrip().split("\t")
+        if len(cols) > 1:
+            word=cols[0]
+            parse=cols[1]
+
+            if word not in parses:
+                parses[word]=[]
+
+            parses[word].append(parse)
+
+    file.close()
+
+
+    file=open(filename)
+    for line in file:        
+        line=re.sub("[\*\[\]/]", "", line)
+        text=line.rstrip().split(" ")
+
+      #  print ' '.join(text)
+        for word in text:
+            #print word
+            parse=""
+            if word in parses:
+                parse=' '.join(parses[word])
+                print "%s%s%s (%s) %s" % (bcolors.REG, word, bcolors.PARSE, parse, bcolors.ENDC),
+            else:
+                print "%s " % (word),
+        print
+
+    file.close()
+
+
+# USAGE python parse text.txt parses.txt
+main(sys.argv[1], sys.argv[2])
